6cd7dd4007553ab308be4c2617d48bf592f1432f
Victor Castelo <victorcbrancom@gmail.com>
2025-10-19 13:59:34 -0300

    add register_animals

--- src/animals.py
+++ src/animals.py
@@ -1,1 +1,9 @@
 list_animals = []
+
+id = 1
+def register_animals(specie, age, weight, status):
+    global id
+    register = {'ID': id,'Espécie': specie ,'Idade': age ,'Peso': weight ,'Status': status}
+    list_animals.append(register)
+    id += 1
+    return list_animals
--- src/main.py
+++ src/main.py
@@ -1,7 +1,49 @@
 from reports import search_animal, search_inputs, search_plant
+def manage_menu():
+    while True:
+        
+        print('Selecione a categoria')
 
-def register_menu():
-    pass
+        print('1.Animais')
+        print('2.Plantas')
+        print('3.Insumos')
+        print('0.Voltar ao menu principal')
+        option = input('\n> ')
+
+        if option == '1':
+            while True:
+                
+                print('O que deseja realizar?')
+
+                print('1.Cadastrar animal')
+                print('2.Atualizar status de animal')
+                print('0.Voltar')
+                choice = input('\n> ')
+
+                if choice == '1':
+                    from animals import register_animals
+                    specie = input('Digite o nome da especie: ')
+                    age = int(input('Digite a idade do animal (ANOS): '))
+                    weight = float(input('Digite o peso do animal (KG): '))
+                    status = input('Digite a situação do animal (ATIVO, VENDIDO OU MORTO): ')
+                    register_animals(specie, age, weight, status)
+
+
+                    print('Animal cadastrado com sucesso!\n')
+                elif choice == '2':
+                    pass
+                elif choice == '0':
+                    break
+                
+        elif option == '2':
+            pass
+        elif option == '3':
+            pass
+        elif option == '0':
+            break
+        else:
+            print('Opção inválida. Tente novamente!')
+            continue
 def report_menu():
     def method_search():
         methods_options = ('1','2')
@@ -27,6 +69,7 @@ def report_menu():
         if option == '1':
             pass
         elif option == '2':
+            
             print('Escolha uma das categorias a ser listada')
             print('1.Animais')
             print('2.Plantas')
@@ -53,8 +96,6 @@ def report_menu():
             print('Voltando...\n')
             break
             
-def manage_input():
-    pass
 def movements_menu():
     pass
 
@@ -67,23 +108,24 @@ while True:
     print(f'{title}\n{line}')
     print('Escolha uma opção')
 
-    print('1.Cadastrar (animais, plantas e insumos)')
+    print('1.Gerenciar animais, plantas e insumos')
     print('2.Gerenciar relatórios')
-    print('3.Gerenciar insumos')
-    print('4.Registrar movimentações')
+    print('3.Registrar movimentações')
     print('0.Sair\n')
 
     option = input('> ')
     if option in options:
         if option == '1':
-            register_menu()
+            manage_menu()
         elif option == '2':
             report_menu()
         elif option == '3':
-            manage_input()
-        elif option == '4':
             movements_menu()
         elif option == '0':
             break    
     else:
         print('Opção inválida. Tente novamente!\n')
+        continue
+
+
+
