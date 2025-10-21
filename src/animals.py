81e55843c0fdde64d1ef91f5a7c26e616fa4b386
Matheussoaresdev <matheussoares.dinizz@gmail.com>
2025-10-20 22:58:18 -0300

    Update_animals
    eu fiz a função de atualiazar os status dos animais

--- src/animals.py
+++ src/animals.py
@@ -3,7 +3,23 @@ list_animals = []
 id = 1
 def register_animals(specie, age, weight, status):
     global id
-    register = {'ID': id,'Espécie': specie ,'Idade': age ,'Peso': weight ,'Status': status}
+    register = {'ID': id,'Specie': specie ,'Age': age ,'Weight': weight ,'Status': status}
     list_animals.append(register)
     id += 1
     return list_animals
+
+def update_animal(new_states, choice):
+             if choice == '1':
+                updates = int(input('Digite o ID: '))   
+                for item in list_animals:
+                    for key in item:
+                      if item[key] == updates:
+                        item['Status'] == new_states
+                      return list_animals
+             elif choice == '2':
+                updates = input('Digite a especie do animal: ')
+                for item in list_animals:
+                     if item[key] == updates:
+                         item['Specie'] == new_states
+                         return list_animals
+                      
