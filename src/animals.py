4c498caaa15116cf7460c2866a684f1455055337
Victor Castelo <victorcbrancom@gmail.com>
2025-10-21 18:11:15 -0300

    fix indentation in update_animals
    ajeitei a indentação da função UPDATE_ANIMALS em ANIMALS.PY

--- src/animals.py
+++ src/animals.py
@@ -8,7 +8,7 @@ def register_animals(specie, age, weight, status):
     id += 1
     return list_animals
 
-def update_animal(new_states):
+def update_animal(new_states, choice):
     if choice == '1':
         updates = int(input('Digite o ID: '))   
         for item in list_animals:
@@ -16,11 +16,11 @@ def update_animal(new_states):
                 if item[key] == updates:
                     item['Status'] = new_states
                     return list_animals
-                elif choice == '2':
-                    updates = input('Digite a especie do animal: ')
-                    for item in list_animals:
-                        for key in item:
-                            if item[key] == updates:
-                                item['Status'] = new_states
-                                return list_animals
+    elif choice == '2':
+        updates = input('Digite a especie do animal: ')
+        for item in list_animals:
+            for key in item:
+                if item[key] == updates:
+                    item['Status'] = new_states
+                    return list_animals
                           
