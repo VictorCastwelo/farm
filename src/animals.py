78fb8cf3760f2de06d07dceaa993640bb31d2c4a
Matheussoaresdev <matheussoares.dinizz@gmail.com>
2025-10-21 17:37:16 -0300

    Update animals.py

--- src/animals.py
+++ src/animals.py
@@ -11,16 +11,16 @@ def register_animals(specie, age, weight, status):
 def update_animal(new_states):
     if choice == '1':
         updates = int(input('Digite o ID: '))   
-            for item in list_animals:
-                for key in item:
-                     if item[key] == updates:
-                        item['Status'] = new_states
-                        return list_animals
-                     elif choice == '2':
-                         updates = input('Digite a especie do animal: ')
-                         for item in list_animals:
-                             for key in item:
-                                 if item[key] == updates:
-                                     item['Status'] = new_states
-                                     return list_animals
+        for item in list_animals:
+            for key in item:
+                if item[key] == updates:
+                    item['Status'] = new_states
+                    return list_animals
+                elif choice == '2':
+                    updates = input('Digite a especie do animal: ')
+                    for item in list_animals:
+                        for key in item:
+                            if item[key] == updates:
+                                item['Status'] = new_states
+                                return list_animals
                           
