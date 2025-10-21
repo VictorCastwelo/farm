348268d34e98a50a88fb3d4cba4371655e436a8b
Lívia Araujo da Silva <liviaaraujosilvatecnologia@gmail.com>
2025-10-20 22:15:57 -0300

    Update plants.py

--- src/plants.py
+++ src/plants.py
@@ -3,7 +3,7 @@ list_plants = []
 id = 1
 def register_plants(crop_type, area, planting_date, harvest_date, status):
     global id
-    register = {'ID': id,'Tipo de cultura': crop_type ,'Área': area ,'Data de plantio': planting_date,'Data da colheita': harvest_date ,'Status': status}
+    register = {'ID': id,'Tipo de cultura': crop_type ,'Área': area ,'Data de plantio': planting_date ,'Data da colheita': harvest_date ,'Status': status}
     list_plants.append(register)
     id += 1
     return list_plants
