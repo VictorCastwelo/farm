914fa8a5cbe800b060830f9c1fd572a409b386ae
Geovana Maria <geovanamariaarjsousa@gmail.com>
2025-10-20 22:20:21 -0300

    Update inputs.py

--- src/inputs.py
+++ src/inputs.py
@@ -3,7 +3,7 @@ list_inputs = []
 id = 1
 def register_inputs(name, quantity, unit, category):
     global id
-    register = {'ID': id,'Nome': name ,'Quantidade': quantity ,'Unidade': unit ,'Categoria': category}
+    register = {'ID': id,'Name': name ,'Quantity': quantity ,'Unit': unit ,'Category': category}
     list_inputs.append(register)
     id += 1
     return list_inputs
