c11839e5ed1e05a621b56c478d57181368d41ee7
Victor Castelo <victorcbrancom@gmail.com>
2025-10-25 13:49:55 -0300

    update register_operations and movements
    falta só converter a lista de movimentações para JSON

--- src/movements.py
+++ src/movements.py
@@ -1,11 +1,23 @@
-def selection_operation(selected, list_object, operation):
-        for item in list_object:
-            for key in item:
-                if item[key] == selected:
-                    item['Status'] = operation
-        return list_object
+from datetime import datetime
+from reports import search
+list_movements = []
 
-    
+def selection_operation(selected, list_object, operation):
+    for item in list_object:
+        for key in item:
+            if item[key] == selected:
+                item['Status'] = operation
+    return list_object
+
+def operation_register(chosen, list_object, description, operation, type_name):
+    item = search(chosen, list_object)
+    date = datetime.now()
+    formatted_date = date.strftime("%Y-%m-%d")
+
+    operation_registration = {'Data': formatted_date, 'ID': item.get('ID'), 'Nome': item.get(type_name), 'Operação': operation, 'Descrição': description}
+
+    list_movements.append(operation_registration)
+    return list_movements
 
 def sell_animal(selected):
     from animals import list_animals
@@ -18,8 +30,12 @@ def harvested_plant(selected):
      return movement
 
 def consumption_input(selected):
-     from inputs import list_inputs
-     movement = selection_operation(selected, list_inputs, operation= 'Consumido')
+     from inputs import list_inputs, out_quantity
+     from reports import search
+     item = search(selected, list_inputs)
+     value_out = item.get('Quantidade')
+
+     movement = out_quantity(value_out, selected)
      return movement
 
 from utils import method_selection
@@ -36,16 +52,23 @@ def movements_menu():
         if option == '1':
             while True:
                 from movements import sell_animal
-                
+                from animals import list_animals
+
                 method = method_selection()
+                description = input('Digite uma descrição para a operação: ')
+
                 if method == '1':
                     chosen_id = int(input('Digite o ID: '))
                     sell_animal(chosen_id)
+                    operation_register(chosen_id, list_animals, description, operation='Venda', type_name='Espécie')
+
                     print('Operação realizada!\n')
                     break
                 elif method == '2':
                     chosen_name = input('Digite o nome da espécie: ')
                     sell_animal(chosen_name)
+                    operation_register(chosen_name, list_animals, description, operation='Venda', type_name='Espécie')
+
                     print('Operação realizada!\n')
                     break
                 else:
@@ -54,16 +77,23 @@ def movements_menu():
         elif option == '2':
             while True:
                 from movements import harvested_plant
-                
+                from plants import list_plants
+
                 method = method_selection()
+                description = input('Digite uma descrição para a operação: ')
+
                 if method == '1':
                     chosen_id = int(input('Digite o ID: '))
                     harvested_plant(chosen_id)
+                    operation_register(chosen_id, list_plants, description, operation='Colheita', type_name='Tipo de cultura')
+
                     print('Operação realizada!\n')
                     break
                 elif method == '2':
                     chosen_name = input('Digite o nome da cultura: ')
                     harvested_plant(chosen_name)
+                    operation_register(chosen_name, list_plants, description, operation='Colheita', type_name='Tipo de cultura')
+
                     print('Operação realizada!\n')
                     break
                 else:
@@ -72,16 +102,23 @@ def movements_menu():
         elif option == '3':
              while True:
                 from movements import consumption_input
-                
+                from inputs import list_inputs
+
                 method = method_selection()
+                description = input('Digite uma descrição para a operação: ')
+
                 if method == '1':
                     chosen_id = int(input('Digite o ID: '))
                     consumption_input(chosen_id)
+                    operation_register(chosen_id, list_inputs, description, operation='Consumo de insumo', type_name='Nome')
+
                     print('Operação realizada!\n')
                     break
                 elif method == '2':
                     chosen_name = input('Digite o nome do insumo: ')
                     consumption_input(chosen_name)
+                    operation_register(chosen_name, list_inputs, description, operation='Consumo de insumo', type_name='Nome')
+
                     print('Operação realizada!\n')
                     break
                 else:
